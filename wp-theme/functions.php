5fa15c0cc242ec16a19cbd9affae4daf466559a5
David Young <david.young@reallysimpleapp.com>
2026-05-26 22:55:05 +0100

    Performance optimizations: replace FontAwesome with inline SVGs, load Google Fonts asynchronously, preload style.css, resize logo.webp

--- wp-theme/functions.php
+++ wp-theme/functions.php
@@ -26,8 +26,6 @@ function cvs_theme_scripts()
     // Enqueue Google Fonts
     wp_enqueue_style('cvs-google-fonts', 'https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&family=Montserrat:wght@700&display=swap', array(), null);
 
-    // Enqueue Font Awesome
-    wp_enqueue_style('cvs-font-awesome', 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css', array(), null);
 
     // Enqueue Main Script
     wp_enqueue_script('cvs-script', get_template_directory_uri() . '/assets/script.js', array(), null, true);
--- wp-theme/header.php
+++ wp-theme/header.php
@@ -42,7 +42,7 @@
         <div class="container">
             <div class="logo">
                 <a href="<?php echo esc_url(home_url('/')); ?>">
-                    <img src="<?php echo get_template_directory_uri(); ?>/assets/logo.webp" alt="CVS Logo" width="900" height="400">
+                    <img src="<?php echo get_template_directory_uri(); ?>/assets/logo.webp" alt="CVS Logo" width="180" height="80">
                 </a>
             </div>
             <nav class="desktop-nav">
@@ -70,6 +70,6 @@
             <div class="header-actions">
                 <a href="<?php echo home_url('/contact'); ?>" class="btn btn-primary">Request a Quote</a>
             </div>
-            <button class="mobile-menu-toggle" aria-label="Menu"><i class="fa-solid fa-bars"></i></button>
+            <button class="mobile-menu-toggle" aria-label="Menu"><svg class="icon-svg" viewBox="0 0 448 512" xmlns="http://www.w3.org/2000/svg"><path fill="currentColor" d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z"/></svg></button>
         </div>
     </header>
